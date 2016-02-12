e135c627c99de8051f873a54413599aabe6c2217
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-02-11 20:07:06 -0500

    social media icons and footer

--- css/style.cs
+++ css/style.cs
@@ -1,3 +1,9 @@
+* {
+  box-sizing: border-box; }
+
+body {
+  overflow-x: hidden; }
+
 .hero-background {
   background: url("../images/PaintSplatterBG.jpeg");
   height: 100vh;
@@ -11,13 +17,39 @@
 .text-center {
   text-align: center; }
 
+nav {
+  position: absolute;
+  top: 30px;
+  right: 10px; }
+
+nav a:link, nav a:active, nav a:visited {
+  color: #888888;
+  text-decoration: none;
+  font-family: "Muli", sans-serif;
+  font-size: 16px;
+  border: 2px solid #888888;
+  padding: 10px;
+  margin: 20px; }
+
+nav a:hover {
+  background-color: #3fb0ac;
+  border-color: #3fb0ac;
+  text-decoration: none;
+  color: #fff;
+  -o-transition: .5s;
+  -ms-transition: .5s;
+  -moz-transition: .5s;
+  -webkit-transition: .5s;
+  transition: .5s; }
+
 .hero-title {
   max-width: 50%;
   color: #fff; }
 
 .first-name {
   font-family: "Philosopher", sans-serif;
-  font-size: 50px; }
+  font-size: 50px;
+  letter-spacing: 5px; }
 
 .last-name {
   font-family: "Vollkorn", serif;
@@ -28,26 +60,57 @@ h1 {
 
 .personal-statement {
   width: 50%;
-  color: #3fb0ac;
+  color: #fff;
   text-align: center;
   font-size: 20px;
-  line-height: 75%;
+  line-height: 120%;
+  letter-spacing: 2px;
   font-family: "Philosopher", sans-serif; }
 
+.down-arrow {
+  padding: 30px; }
+
+.arrow-size {
+  width: 50px; }
+
+.down-arrow:hover {
+  opacity: .8;
+  -o-transition: .5s;
+  -ms-transition: .5s;
+  -moz-transition: .5s;
+  -webkit-transition: .5s;
+  transition: .5s; }
+
 .portfolio {
-  height: 400px;
   overflow: hidden;
-  padding-top: 20px;
-  padding-bottom: 20px; }
+  width: 100%; }
+
+.padding-top {
+  padding-top: 30px; }
+
+.padding {
+  padding-top: 30px;
+  padding-bottom: 30px; }
+
+.portfolio:hover {
+  opacity: .8;
+  -o-transition: .5s;
+  -ms-transition: .5s;
+  -moz-transition: .5s;
+  -webkit-transition: .5s;
+  transition: .5s; }
 
 .about-me {
-  background-color: #dddfd4;
+  background-color: #eeeeee;
   width: 100%; }
 
 .headshot {
   height: 300px;
   padding-top: 50px;
-  opacity: .95; }
+  opacity: .95;
+  filter: grayscale(100%);
+  -webkit-filter: grayscale(100%);
+  filter: grayscale(100%); }
 
 .content-title {
   font-family: "Vollkorn", serif;
@@ -55,6 +118,17 @@ h1 {
   color: #173e43;
   padding-top: 30px;
   padding-bottom: 30px;
-  border-bottom: #fae596; }
+  border-bottom: 2px solid #3fb0ac;
+  margin-bottom: 30px; }
+
+.caitlin-description {
+  font-family: "Muli", sans-serif;
+  font-color: #173e43; }
+
+.social-media-icons {
+  width: 30px; }
+
+.footer {
+  background-color: #173e43; }
 
 /*# sourceMappingURL=style.cs.map */
